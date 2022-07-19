199e7f50ca258c912775293f5017ef23023689d5
Aiden <myjingu@naver.com>
2022-07-19 22:41:45 +0900

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const { User } = require("./models/User");
-const config =require('./config/key');
+const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,16 +21,47 @@ app.get('/', (req, res) => res.send('Helloo'))
 // 회원가입
 app.post('/register', (req, res) => {
     // 회원가입할때 필요한 정보들을 client 에서 가져오면 그것들을 DB에 넣어준다.
-
-
     const user = new User(req.body)
 
     user.save((err, userInfo) => {
-        if(err) return res.json({ success: false, err})
+        if (err) return res.json({ success: false, err })
         return res.status(200).json({
             success: true
         })
     })
 
 })
+
+// 로그인
+app.post('/login', (req, res) => {
+    // 요청된 이메일 DB에서 찾기
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "해당 유저가 없습니다."
+            })
+        }
+        // 이메일이 있다면 비밀번호가 맞는지
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+
+            // 위에 상황이 다 성공하면 토큰 생성
+            user.generateToken((err, user) => {
+
+            })
+        })
+    })
+
+
+})
+
+
+
+
+
+
+
+
 app.listen(port, () => console.log(`on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,41 +1,71 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const userSChema = mongoose.Schema({
-    name : {
-        type : String,
+    name: {
+        type: String,
         maxlength: 50,
-        
+
     },
-    email :{
+    email: {
         type: String,
         trim: true,
         unique: 1
     },
-    
-    password : {
-        type : String,
+
+    password: {
+        type: String,
         minlength: 5
     },
-    lastname : {
+    lastname: {
         type: String,
         maxlength: 50
     },
     role: {
         type: Number,
-        default : 0
+        default: 0
     },
     imgae: String,
-    token : {
-        type : String
+    token: {
+        type: String
     },
     tokenExp: {
         type: Number
     },
-    Gender :{
-        type:String
+    Gender: {
+        type: String
     },
-    
+
 })
+
+userSChema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        // 비밀번호 암호화
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+
+
+
+})
+
+userSChema.methods.comparePassword = function (plainPassword, cb) {
+
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        cb(null, isMatch)
+    })
+}
 
 const User = mongoose.model('User', userSChema)
 
